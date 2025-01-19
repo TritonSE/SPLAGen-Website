8d4c681df861259c282f2bbc00a5fdd3ec1d294a
pratyush1718 <pratyush.chand@outlook.com>
2025-01-19 09:26:26 -0800

    user routes & controllers

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
+import userRoutes from './routes/user'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
