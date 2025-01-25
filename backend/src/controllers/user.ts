3736d23394afbbdd6dd5344bb4ed2ec4de21bf4d
pratyush1718 <pratyush.chand@outlook.com>
2025-01-25 09:54:20 -0800

    comment changes

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,65 @@
+import { RequestHandler } from "express";
+
+export const getPersonalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Get personal information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editPersonalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Edit personal information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProfessionalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Get professional information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editProfessionalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Edit professional information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getDirectoryPersonalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Get directory personal information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editDirectoryPersonalInformation: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Edit directory personal information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getDirectoryDisplayInfo: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Get directory display information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editDirectoryDisplayInfo: RequestHandler = async (req, res, next) => {
+  try {
+    res.status(200).send("Edit directory display information route works!");
+  } catch (error) {
+    next(error);
+  }
+};
