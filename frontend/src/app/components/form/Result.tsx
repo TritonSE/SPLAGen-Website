3ac8582a9c2f55d1475698fc0886eaee08ddc29a
Adam Rolander <adamsrolander2006@gmail.com>
2025-01-29 15:41:07 -0800

    Removed unnecessary lint ignores

--- frontend/src/app/components/form/Result.tsx
+++ frontend/src/app/components/form/Result.tsx
@@ -1,18 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useStateMachine } from "little-state-machine";
+import { useNavigate } from "react-router-dom";
 
 import updateAction from "../../state/updateAction";
 
 const Result = () => {
   const { state } = useStateMachine({ actions: { updateAction } });
-  console.log(state.data);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    void navigate("/");
+  };
 
   return (
     <div>
       <h2>Review Your Information</h2>
       <pre>{JSON.stringify(state.data, null, 2)}</pre>
+      <button onClick={goBack}>Back</button>
     </div>
   );
 };
