039dc0acc8508dd27a4788c79c99b3847dd2cd8e
Unnati Goyal <ugoyal0602@gmail.com>
2025-02-01 14:57:33 -0800

    Fixed the design on Post Modal and Directory Modal

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        mainHeading: "#3B3B62",
+        disabled: "#C0C8CB",
       },
     },
   },
