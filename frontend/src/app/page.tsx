28663f01af94ba1d61f88a326773c1d7d53b8aca
Helen <59126064+Miyuki-L@users.noreply.github.com>
2025-02-28 00:11:28 -0800

    frontend(onboardingForm): rebase and removed onboardingFormWrapper

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,6 @@
 import LanguageSwitcher from "../components/languageSwitcher";
 import External from "../components/translationDemo";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Result from "./components/form/Result";
-import Step1 from "./components/form/Step1";
-import "./state/store";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen">
--- frontend/src/components/onboardingForm/index.ts
+++ frontend/src/components/onboardingForm/index.ts
@@ -3,4 +3,3 @@ export { Step1 } from "./Step1";
 export { Step2 } from "./Step2";
 export { Step3A } from "./Step3A";
 export { Step3B } from "./Step3B";
-export { OnboardingFormWrapper } from "./onboardingFormWrapper";
