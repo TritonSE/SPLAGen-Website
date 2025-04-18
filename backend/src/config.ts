906a105dcfdd0c56dd2e2b863d7539359dc77490
r800360 <rosachdeva@ucsd.edu>
2025-04-17 17:59:31 -0700

    Configuration fixes and simplifications

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,46 +1,6 @@
 import env from "./util/validateEnv";
 
-export type FirebaseConfig = {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_key: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-  universe_domain: string;
-};
-
-// Parse the Firebase configuration JSON string
-let firebaseConfig: FirebaseConfig;
-try {
-  firebaseConfig = JSON.parse(env.BACKEND_FIREBASE_SETTINGS as string) as FirebaseConfig;
-} catch (error) {
-  console.log(error);
-  throw new Error("Error parsing BACKEND_FIREBASE_SETTINGS in the .env file:");
-}
-
-// Old working configuration is important for deployment purposes and left for reference:
-// Firebase configuration from environment variables
-// const firebaseConfig = {
-//   projectId: throwIfUndefined(
-//     process.env.PROJECT_ID,
-//     new Error("PROJECT_ID is missing in the .env"),
-//   ),
-//   clientEmail: throwIfUndefined(
-//     process.env.CLIENT_EMAIL,
-//     new Error("CLIENT_EMAIL is missing in the .env"),
-//   ),
-//   privateKey: throwIfUndefined(
-//     process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//     new Error("PRIVATE_KEY is missing or incorrectly formatted in the .env"),
-//   ),
-// };
-
 const mongoURI = env.MONGODB_URI;
 const port = env.PORT;
 
-export { firebaseConfig, mongoURI, port };
+export { mongoURI, port };
--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -3,8 +3,11 @@
  * If any .env parameters are missing, the server will not start and an error will be thrown.
  */
 
+import dotenv from "dotenv";
 import { cleanEnv } from "envalid";
 import { email, json, port, str } from "envalid/dist/validators";
+
+dotenv.config();
 
 export default cleanEnv(process.env, {
   PORT: port(), // Port to run backend on
@@ -12,7 +15,6 @@ export default cleanEnv(process.env, {
   FRONTEND_ORIGIN: str(), // URL of frontend, to allow CORS from frontend
   EMAIL_USER: email(), // Email address to use for sending emails
   EMAIL_APP_PASSWORD: str(), // App password to use for sending emails
-  EMAIL_NOTIFICATIONS_RECIPIENT: email(), // Recipient of VSR notification emails
   BACKEND_FIREBASE_SETTINGS: json(), // Firebase settings for backend, stored as a JSON string
   SERVICE_ACCOUNT_KEY: json(), // Private service account key for backend, stored as a JSON string
 });
