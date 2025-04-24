3f8c7614a024321f2bee54531d83e5621de34a91
kathyychenn <ktchen@ucsd.edu>
2025-04-24 16:27:18 -0700

    merge main and add icons

---
+++ frontend/src/app/demos/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import React, { useContext, useState } from "react";
+
+import { EditBasicInfoModal, External, LanguageSwitcher } from "@/components";
+import { UserContext } from "@/contexts/userContext";
+
+export default function Dashboard() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const { user } = useContext(UserContext);
+  return (
+    <div>
+      <h1> Dashboard/Home </h1>
+      <p>
+        <Link href="/login">go to login</Link>
+      </p>
+      <p>
+        <Link href="/phoneInputDemo"> Go to phone input demo </Link>
+      </p>
+      <p>
+        <Link href="flowPopupDemo"> Go to flow popup demo </Link>
+      </p>
+      <p>
+        <Link href="dashboard"> Go to dashboard demo </Link>
+      </p>
+      <p>
+        <Link href="/profile"> Go to flow profile page WIP demo </Link>
+      </p>
+
+      <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen">
+        <main className="flex flex-col gap-8 row-start-2 items-center">
+          {/* Added the LanguageSwitcher component */}
+          <LanguageSwitcher />
+          {/* External is my text component */}
+          <External></External>
+
+          {user && (
+            <p>
+              {user.personal.firstName} {user.role}
+            </p>
+          )}
+          <button onClick={handleOpenModal}>Open Post Modal</button>
+          {/* <DirectoryInfoModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
+          <EditBasicInfoModal isOpen={isModalOpen} onClose={handleCloseModal} />
+          {/* <CreatePostModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
+          {/* <ProfessionalInfoModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
+        </main>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,57 +1,76 @@
 "use client";
-import Link from "next/link";
-import React, { useContext, useState } from "react";
+// import React, { useContext } from "react";
 
-import { EditBasicInfoModal, External, LanguageSwitcher } from "@/components";
-import { UserContext } from "@/contexts/userContext";
+// import { UserContext } from "@/contexts/userContext";
+import "./dashboard.css";
 
 export default function Dashboard() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const { user } = useContext(UserContext);
+  // const { user } = useContext(UserContext);
   return (
-    <div className="p-4">
-      <h1> Dashboard/Home </h1>
-      <p>
-        <Link href="/login">go to login</Link>
-      </p>
-      <p>
-        <Link href="/phoneInputDemo"> Go to phone input demo </Link>
-      </p>
-      <p>
-        <Link href="flowPopupDemo"> Go to flow popup demo </Link>
-      </p>
-      <p>
-        <Link href="/profile"> Go to flow profile page WIP demo </Link>
-      </p>
-
-      <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen">
-        <main className="flex flex-col gap-8 row-start-2 items-center">
-          {/* Added the LanguageSwitcher component */}
-          <LanguageSwitcher />
-          {/* External is my text component */}
-          <External></External>
-
-          {user && (
-            <p>
-              {user.personal.firstName} {user.role}
-            </p>
-          )}
-          <button onClick={handleOpenModal}>Open Post Modal</button>
-          {/* <DirectoryInfoModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
-          <EditBasicInfoModal isOpen={isModalOpen} onClose={handleCloseModal} />
-          {/* <CreatePostModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
-          {/* <ProfessionalInfoModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
-        </main>
+    <div className="dash">
+      <h1 className="title"> Dashboard </h1>
+      <div className="resources">
+        <h2 className="resourcesH2">Resources</h2>
+        <div className="resourceCards">
+          <a
+            href="https://drive.google.com/drive/folders/1yv2D8KsLlUgcRScUS86G_DOOzUiXM3ln?usp=drive_link"
+            target="_blank"
+            className="resourceCard"
+          >
+            <div className="cardInfo">
+              <img src="/images/educational.svg" alt="educationIcon"></img>
+              <div className="cardText">
+                <h3 className="cardTitle">Educational</h3>
+                <p className="cardDescription">This is a description of education.</p>
+              </div>
+            </div>
+            <span className="rectangle"></span>
+          </a>
+          <a
+            href="https://drive.google.com/drive/folders/1Xxd0MRpT-RBxMVUakFr9ykWz6qzitLnX?usp=drive_link"
+            target="_blank"
+            className="resourceCard"
+          >
+            <div className="cardInfo">
+              <img src="/images/publicPolicy.svg" alt="publicIcon"></img>
+              <div className="cardText">
+                <h3 className="cardTitle">Public Policy</h3>
+                <p className="cardDescription">This is a description of public policy.</p>
+              </div>
+            </div>
+            <span className="rectangle"></span>
+          </a>
+          <a
+            href="https://drive.google.com/drive/folders/1LJ4UUgTKIUS6Bj8bip0hj2rQBaetzWsb"
+            target="_blank"
+            className="resourceCard"
+          >
+            <div className="cardInfo">
+              <img src="/images/research.svg" alt="researchIcon"></img>
+              <div className="cardText">
+                <h3 className="cardTitle">Research</h3>
+                <p className="cardDescription">This is a description of research.</p>
+              </div>
+            </div>
+            <span className="rectangle"></span>
+          </a>
+          <a
+            href="https://drive.google.com/drive/folders/1D_0kRA4leoOnFMjdvlF5YMdt1x6KGxjU?usp=drive_link"
+            target="_blank"
+            className="resourceCard"
+          >
+            <div className="cardInfo">
+              <img src="/images/members.svg" alt="membersIcon"></img>
+              <div className="cardText">
+                <h3 className="cardTitle">Members</h3>
+                <p className="cardDescription">This is a description of members.</p>
+              </div>
+            </div>
+            <span className="rectangle"></span>
+          </a>
+        </div>
       </div>
+      <h2 className="resourcesH2">Recent Announcements</h2>
     </div>
   );
 }
