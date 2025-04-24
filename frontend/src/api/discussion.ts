5ce57771da5b7780bd9ec23c4aae24ba4a91d94f
Unnati Goyal <ugoyal0602@gmail.com>
2025-04-24 15:46:38 -0700

    fixed button styling and backend

--- frontend/src/api/discussion.ts
+++ frontend/src/api/discussion.ts
@@ -1,12 +1,25 @@
 import { APIResult, handleAPIError, post } from "./requests";
 
+type Discussion = {
+  _id: string;
+  userId: string;
+  title: string;
+  message: string;
+  channel: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
 export const createPost = async (postData: {
   title: string;
   message: string;
-}): Promise<APIResult<null>> => {
+}): Promise<APIResult<Discussion>> => {
   try {
-    await post("/api/discussion", postData);
-    return { success: true, data: null };
+    const response: Response = await post("/api/discussions", postData);
+    const data = (await response.json()) as Discussion;
+
+    return { success: true, data };
   } catch (error) {
     return handleAPIError(error);
   }
