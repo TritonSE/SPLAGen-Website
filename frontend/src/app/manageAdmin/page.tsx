88f049361f4642b6097871091dded36e2a4983cf
Kiruthika Marikumaran <kiruthika.star14@gmail.com>
2025-05-15 14:36:36 -0700

    moving popup

--- frontend/src/app/manageAdmin/page.tsx
+++ frontend/src/app/manageAdmin/page.tsx
@@ -86,7 +86,7 @@ const ManageAdmin: React.FC = () => {
     const index = dummyAdmins.findIndex((r) => r.id === row.id);
     if (index !== -1) setSelectedIndex(index);
   }, []);
-
+  /*Moving the popup*/ 
   const handlePrev = () => {
     if (selectedIndex !== null) {
       setSelectedIndex((prev) => {
